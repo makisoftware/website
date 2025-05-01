487a08990cdedb8cfb13632a5ced7de1f5a615ce
Elliott Etzkorn <etz@wikimedia.org>
2025-05-01 21:09:54 +0300

    Update image path

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -10,7 +10,7 @@
 <div class="level-two-container">
     <div class="level-two-text-area">
         <h1>Projects</h1>
-        <Project link={IMESSAGE_LINK} imagePath="/new.png" title="New" description="Launching soon, join the iMessage group to stay tuned" />
+        <Project link={IMESSAGE_LINK} imagePath="/new.jpeg" title="New" description="Launching soon, join the iMessage group to stay tuned" />
         <Project link="https://t4t.social" imagePath="/t4t.png" title="t4t" description="Community for trans and gender-non-comforming people" />
         <Project link="https://pingpractice.org/" imagePath="/practice.png" title="Ping Practice" description="Miniature bits of mindfulness" />
         <Project link="https://dwelling.love/" imagePath="/dwelling.png" title="Dwelling" description="A social network for your home" />
