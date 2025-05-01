e67b6204f433a128fee445c8e72929922c89849a
Elliott Etzkorn <etz@wikimedia.org>
2025-05-01 16:36:01 +0300

    Add new project too

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -6,6 +6,7 @@
 <div class="level-two-container">
     <div class="level-two-text-area">
         <h1>Projects</h1>
+        <Project link="" imagePath="/new.png" title="New" description="Launching soon, join the iMessage group to stay tuned" />
         <Project link="https://t4t.social" imagePath="/t4t.png" title="t4t" description="Social network for trans and gender-non-comforming people" />
         <Project link="https://pingpractice.org/" imagePath="/practice.png" title="Ping Practice" description="Miniature bits of mindfulness" />
         <Project link="https://dwelling.love/" imagePath="/dwelling.png" title="Dwelling" description="A social network for your home" />
