1126b45e63c37669698005f1f9dee0799f35a205
Elliott Etzkorn <etz@wikimedia.org>
2025-05-03 23:26:18 +0300

    2g

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -10,7 +10,7 @@
 <div class="level-two-container">
     <div class="level-two-text-area">
         <h1>Projects</h1>
-        <Project link={IMESSAGE_LINK} imagePath="/new.jpeg" title="New" description="Launching soon, join the iMessage group to stay tuned" />
+        <Project link={IMESSAGE_LINK} imagePath="/2g.jpeg" title="2g" description="Launching soon, join the iMessage group to stay tuned" />
         <Project link="https://t4t.social" imagePath="/t4t.jpeg" title="t4t" description="Community for trans and gender-non-comforming people" />
         <Project link="https://pingpractice.org/" imagePath="/practice.jpeg" title="Ping Practice" description="Miniature bits of mindfulness" />
         <Project link="https://dwelling.love/" imagePath="/dwelling.jpeg" title="Dwelling" description="A social network for your home" />
