0e5e609b38d1f6f2efd82d74b2188d9e95487f12
etz <etz@wikimedia.org>
2025-05-24 11:04:34 -0400

    Simplify contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -11,13 +11,11 @@
     <div class="level-two-text-area">
         <h1>Contact</h1>
         <p>
-            Write us at <a href="mailto:hey@makisoftware.com">hey@makisoftware.com</a>
+            Write us at <a href="mailto:hey@makisoftware.com">hey@makisoftware.com</a>.
             <br/><br/>
-            Join the iMessage <a href={IMESSAGE_LINK}>group chat</a>
+            Join the <a href={IMESSAGE_LINK}>iMessage</a> group chat.
             <br/><br/>
-            Reach out to <a href="https://elliottetzkorn.com">Elliott</a>
-            <br/><br/>
-            Or find us in the Catskills
+            Or find us in <a href="https://en.wikipedia.org/wiki/Catskill_Mountains">The Catskills</a>.
         </p>
     </div>
 </div>
