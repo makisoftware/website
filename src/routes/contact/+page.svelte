4f352449800fc3cafbb7a3991c304f4388f84b9f
Elliott Etzkorn <etz@wikimedia.org>
2025-08-13 16:08:12 -0400

    Update the contact for a mailing list

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -8,7 +8,7 @@
         <p>
             Write us at <a href="mailto:mail@makisoftware.com">mail@makisoftware.com</a>
             <br/><br/>
-            Join the WhatsApp <a href="https://chat.whatsapp.com/GS1pNpaWLZE3wbTrJf9CyO">group chat</a>
+            Join the <a href="https://buttondown.com/makisoft">mailing list</a>
             <br/><br/>
             Or find us in the <a href="https://en.wikipedia.org/wiki/Catskill_Mountains">Catskill Mountains</a>
         </p>
