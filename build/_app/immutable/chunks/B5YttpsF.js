1d29cf2d4ca94f771d734349ba4a8e1231272efa
Elliott Etzkorn <mail@elliottetzkorn.com>
2026-05-02 18:31:29 +0200

    Add callback redirect server for

---
+++ build/_app/immutable/chunks/B5YttpsF.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1wpdeon?.base??"",e=globalThis.__sveltekit_1wpdeon?.assets??s;export{e as a,s as b};
--- build/_app/immutable/chunks/CltUAa0_.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_jv9lap?.base??"",a=globalThis.__sveltekit_jv9lap?.assets??s;export{a,s as b};
