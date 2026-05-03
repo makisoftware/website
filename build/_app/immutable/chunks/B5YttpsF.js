6bc7d725ca86ae2efad7136141d13c53048c8b24
Elliott Etzkorn <mail@elliottetzkorn.com>
2026-05-03 17:11:42 +0200

    Asdf

--- build/_app/immutable/chunks/B5YttpsF.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1wpdeon?.base??"",e=globalThis.__sveltekit_1wpdeon?.assets??s;export{e as a,s as b};
---
+++ build/_app/immutable/chunks/CI-3R7l6.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_51270w?.base??"",a=globalThis.__sveltekit_51270w?.assets??s;export{a,s as b};
